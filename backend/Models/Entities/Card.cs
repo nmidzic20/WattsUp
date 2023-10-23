d168e67b2973f7faed75f6b76e8885d7e552eefb
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-10-23 23:00:12 +0200

    ANP-38 Created models of entities

---
+++ backend/Models/Entities/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public virtual User OwnedBy { get; set; }
+        public string Value { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Event> Events { get; set; }
+    }
+}
---
+++ backend/Models/Entities/Event.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models.Entities
+{
+    [PrimaryKey(nameof(ChargerId), nameof(CardId))]
+    public class Event
+    {
+        public int ChargerId { get; set; }
+        public int CardId { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime EndedAt { get; set; }
+        public double VolumeKwh { get; set; }
+
+        public virtual Charger Charger { get; set; }
+        public virtual Card Card { get; set; }
+    }
+}
