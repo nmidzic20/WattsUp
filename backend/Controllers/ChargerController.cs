608cb4a34a5a814f4692b894f5280c66835988e9
Hrvoje Lukšić <hluksic20@foi.hr>
2023-10-24 15:11:11 +0200

    ANP-34 Implemented charger controller class with AddCharger method

---
+++ backend/Controllers/ChargerController.cs
@@ -0,0 +1,51 @@
+﻿using backend.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChargerController : Controller
+    {
+        private readonly WattsUpDbContext _dbContext;
+        private HttpClient _client;
+
+        public ChargerController(WattsUpDbContext dbContext, HttpClient httpClient)
+        {
+            _dbContext = dbContext;
+            _client = httpClient;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Charger>> AddCharger([FromBody] Charger _charger)
+        {
+            var charger = new Charger
+            {
+                Name = _charger.Name,
+                Latitude = _charger.Latitude,
+                Longitude = _charger.Longitude,
+                CreatedAt = DateTime.Now,
+                CreatedBy = _charger.CreatedBy,
+                LastSyncAt = DateTime.Now,
+                Active = false
+            };
+
+            _dbContext.Sensors.Add(charger);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new Charger
+            {
+                Id = charger.Id,
+                Name = charger.Name,
+                Latitude = charger.Latitude,
+                Longitude = charger.Longitude,
+                CreatedAt = charger.CreatedAt,
+                CreatedBy = charger.CreatedBy,
+                LastSyncAt = charger.LastSyncAt,
+                Active = charger.Active
+            });
+        }
+    }
+}
