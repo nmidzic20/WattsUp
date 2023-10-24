8c896ffb5bcbc790c1c156e4bc920f6504cde31a
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-10-24 14:06:05 +0200

    ANP-39 Added context and connection to database

---
+++ backend/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Event> Event { get; set; }
+        public DbSet<Charger> Charger { get; set; }
+        public DbSet<Card> Card { get; set; }
+        public DbSet<Role> Role { get; set; }
+
+    }
+}
