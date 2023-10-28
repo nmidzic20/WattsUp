7763baed6dfa9645d1e9dedca2fa67fcee4cf410
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-10-29 01:12:17 +0200

    ANP-35 Created POST method for adding User in database and added basic validation of incoming data

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models.Entities;
+using backend.Models.Requests;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public UsersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(UserCreateRequest userRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingUser = await _context.User.FirstOrDefaultAsync(u => u.Email == userRequest.Email);
+            if (existingUser != null)
+            {
+                return Conflict(new { message = "A user with this email already exists." });
+            }
+
+            User newUser = new User
+            {
+                FirstName = userRequest.FirstName,
+                LastName = userRequest.LastName,
+                Email = userRequest.Email,
+                Password = userRequest.Password,
+                Active = true,
+                CreatedAt = DateTime.UtcNow,
+                RoleId = 2
+            };
+
+            _context.User.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = $"User {newUser.FirstName} is added to database!" });
+        }
+    }
+}
---
+++ backend/Models/Requests/UserCreateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Requests
+{
+    public class UserCreateRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
