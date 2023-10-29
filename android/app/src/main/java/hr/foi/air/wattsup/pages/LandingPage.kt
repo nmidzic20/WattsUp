ee18c7929f2aa6e082676e74503acea532debdd6
Noa Midzic <nmidzic@yahoo.com>
2023-10-29 15:47:39 +0100

    ANP-23 Spinning logo added to top app bar next to app title

--- android/app/src/main/java/hr/foi/air/wattsup/pages/LandingPage.kt
+++ android/app/src/main/java/hr/foi/air/wattsup/pages/LandingPage.kt
@@ -1,12 +1,21 @@
 package hr.foi.air.wattsup.pages
 
+import android.os.Build
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.material3.* // ktlint-disable no-wildcard-imports
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
 import hr.foi.air.wattsup.R
 import hr.foi.air.wattsup.ui.component.ModeButton
 
@@ -17,7 +26,56 @@ fun LandingPage(onChargerModeClick: () -> Unit) {
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(stringResource(R.string.app_name))
+                    /*Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        // Spacer(Modifier.width(8.dp))
+                        Text(
+                            text = stringResource(R.string.app_name),
+                            modifier = Modifier.padding(start = 8.dp), // Add padding to the text
+                            style = MaterialTheme.typography.titleLarge,
+                            color = MaterialTheme.colorScheme.onPrimary,
+                        )
+
+                        // Spacer(modifier = Modifier.width(8.dp))
+                        SpinningLogo()
+                    }*/
+                    BoxWithConstraints(
+                        contentAlignment = Alignment.CenterStart,
+                        modifier = Modifier
+                            .width(170.dp),
+                        // .background(color = Color.Blue),
+                    ) {
+                        val boxWidth = this.maxWidth
+
+                        Box(
+                            modifier = Modifier
+                                .width(boxWidth - 120.dp)
+                                // .background(Color.Red)
+                                .align(Alignment.CenterStart),
+                        ) {
+                            SpinningLogo()
+                        }
+
+                        Row {
+                            // Spacer(modifier = Modifier.width(100.dp))
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier
+                                    .width(boxWidth),
+                                // .zIndex(-2f)
+                                // .background(Color.Yellow),
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.app_name),
+                                    modifier = Modifier.padding(start = 8.dp), // Add padding to the text
+                                    style = MaterialTheme.typography.titleLarge,
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                )
+                            }
+                        }
+                    }
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -54,8 +112,32 @@ fun LandingPage(onChargerModeClick: () -> Unit) {
     }
 }
 
+@Composable
+fun SpinningLogo() {
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (Build.VERSION.SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = R.drawable.icon_yellow).apply(block = {
+                size(100, 100)
+            }).build(),
+            imageLoader = imageLoader,
+        ),
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth(),
+    )
+}
+
 @Preview
 @Composable
 fun LandingPagePreview() {
-    LandingPage({})
+    LandingPage {}
 }
