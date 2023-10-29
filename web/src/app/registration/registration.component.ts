3589dfde66fbf4a343c8d30bb56fc521bc378459
Karlo Vardijan <kvardijan20@foi.hr>
2023-10-29 17:37:43 +0100

    ANP-50 Initialized variables for registration input elements

--- web/src/app/registration/registration.component.ts
+++ web/src/app/registration/registration.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+  firstNameInput?: HTMLInputElement;
+  lastNameInput?: HTMLInputElement;
+  emailInput?: HTMLInputElement;
+  passwordInput?: HTMLInputElement;
 
+  constructor(private router: Router){
+
+  }
+
+  ngOnInit(): void {
+      this.firstNameInput = document.getElementById("firstname") as HTMLInputElement;
+      this.lastNameInput = document.getElementById("lastname") as HTMLInputElement;
+      this.emailInput = document.getElementById("email") as HTMLInputElement;
+      this.passwordInput = document.getElementById("password") as HTMLInputElement;
+  }
 }
