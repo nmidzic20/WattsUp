1550f13168e79c500b21ac4878df3181dd62acef
Noa Midzic <nmidzic@yahoo.com>
2023-10-31 17:55:40 +0100

    ANP-23 Bolded text within CircleButton

--- android/app/src/main/java/hr/foi/air/wattsup/ui/component/CircleButton.kt
+++ android/app/src/main/java/hr/foi/air/wattsup/ui/component/CircleButton.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,6 +61,7 @@ fun CircleButton(mode: String, onClick: () -> Unit, color: Color?, iconId: Int?)
 
                 Text(
                     text = mode,
+                    fontWeight = FontWeight.Bold,
                     style = if (iconId != null) MaterialTheme.typography.bodyLarge else MaterialTheme.typography.titleLarge,
                     textAlign = TextAlign.Center,
                 )
