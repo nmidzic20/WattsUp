9162073cd53eb6fd13bbc6d790c30959b42dd6f8
Karlo Vardijan <kvardijan20@foi.hr>
2023-11-04 19:16:30 +0100

    ANP-51 Imported FormsModule for form validation, old validation method replaced with NgForm validation

--- web/src/app/registration/registration.component.ts
+++ web/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,54 +8,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit{
-  firstNameInput?: HTMLInputElement;
-  lastNameInput?: HTMLInputElement;
-  emailInput?: HTMLInputElement;
-  passwordInput?: HTMLInputElement;
-  messageBox?: HTMLElement;
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  message: string = '';
 
   constructor(private router: Router){
 
   }
 
   ngOnInit(): void {
-      this.firstNameInput = document.getElementById("firstname") as HTMLInputElement;
-      this.lastNameInput = document.getElementById("lastname") as HTMLInputElement;
-      this.emailInput = document.getElementById("email") as HTMLInputElement;
-      this.passwordInput = document.getElementById("password") as HTMLInputElement;
-      this.messageBox = document.getElementById("message") as HTMLElement;
   }
 
-  validateInput(){
-    let isValid = true;
-    let firstname = this.firstNameInput?.value;
-    let lastname = this.lastNameInput?.value;
-    let email = this.emailInput?.value;
-    let password = this.passwordInput?.value;
-    let errorMessage = "";
-
-    if (firstname == ""){
-      isValid = false;
-      errorMessage += "First name field is empty!";
-    } 
-    if (lastname == ""){
-      isValid = false;
-      errorMessage += "Last name field is empty!";
-    } 
-    if (email == ""){
-      isValid = false;
-      errorMessage += "Email field is empty!";
-    } 
-    if (password == ""){
-      isValid = false;
-      errorMessage += "Password field is empty!";
+  submitRegistration(form: NgForm){
+    if(form.valid){
+      this.message = "successfully submited";
+    }else{
+      this.message = "not valid"
     }
-    if (password!!.length < 6){
-      isValid = false;
-      errorMessage += "Password must be at least 6 characters long!";
-    }
-    this.messageBox!!.innerHTML = errorMessage;
-    
-    return isValid;
   }
 }
