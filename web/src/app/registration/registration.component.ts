17475c3b871d46a121fda72d47e2ec681b00c54d
Karlo Vardijan <kvardijan20@foi.hr>
2023-11-05 01:49:33 +0100

    ANP-51 Implemented function that utilizes ngForms and writes error messages

--- web/src/app/registration/registration.component.ts
+++ web/src/app/registration/registration.component.ts
@@ -8,24 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit{
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  password: string = '';
-  message: string = '';
+  errorMessageBox? : HTMLElement;
 
   constructor(private router: Router){
 
   }
 
   ngOnInit(): void {
+    this.errorMessageBox = document.getElementById("message") as HTMLElement;
   }
 
   submitRegistration(form: NgForm){
     if(form.valid){
-      this.message = "successfully submited";
+      this.errorMessageBox!!.innerHTML = "Success!";
+      //Implement endpoint fetch
     }else{
-      this.message = "not valid"
+      this.errorMessageBox!!.innerHTML = this.showErrorMessage(form);
     }
+  }
+
+  showErrorMessage(form: NgForm){
+    let errMsg = "";
+    let firstName = form.controls['firstname'].errors;
+    let lastName = form.controls['lastname'].errors;
+    let email = form.controls['email'].errors;
+    let password = form.controls['password'].errors;
+
+    if(firstName?.['required']) errMsg += "First name is required! <br>";
+    if(lastName?.['required']) errMsg += "Last name is required! <br>";
+    if(email?.['required']) errMsg += "Email is required! <br>";
+    if(email?.['email']) errMsg += "Email is not valid! <br>";
+    if(password?.['required']) errMsg += "Password is required! <br>";
+    if(password?.['minlength']) errMsg += "Password has to be at least 6 characters long!";
+
+    return errMsg;
   }
 }
