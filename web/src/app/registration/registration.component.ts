0e39638e68b39fd859fcc29cbb2d098755bcd105
Karlo Vardijan <kvardijan20@foi.hr>
2023-11-07 16:15:28 +0100

    ANP-51 Fixed the assignment of lastName, email and password variables

--- web/src/app/registration/registration.component.ts
+++ web/src/app/registration/registration.component.ts
@@ -21,9 +21,9 @@ export class RegistrationComponent implements OnInit{
   async submitRegistration(form: NgForm){
     if(form.valid){
       let firstName = form.controls['firstname'].value;
-      let lastName = form.controls['firstname'].value;
-      let email = form.controls['firstname'].value;
-      let password = form.controls['firstname'].value;
+      let lastName = form.controls['lastname'].value;
+      let email = form.controls['email'].value;
+      let password = form.controls['password'].value;
       let header = new Headers();
 
       header.set("Content-Type", "application/json");
