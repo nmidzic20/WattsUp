225f3d6d03f2189db2c8293eeb85aabe6d7d11a5
Karlo Vardijan <kvardijan20@foi.hr>
2023-11-08 21:01:02 +0100

    ANP-53 Implemented routes for login, login set as landing page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'registration', component: RegistrationComponent},
-  { path: '**', component: NotFoundComponent}
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
