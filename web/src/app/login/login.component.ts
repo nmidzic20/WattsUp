5ca04561b95fbd8be9e48288a64870a1aaacb029
Karlo Vardijan <kvardijan20@foi.hr>
2023-11-12 13:10:13 +0100

    ANP-54 Imported NgForm and Router, declared error message element

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  errorMessageBox? : HTMLElement;
 
+  constructor(private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    this.errorMessageBox = document.getElementById("message") as HTMLElement;
+  }
+
+  async submitLogin(form: NgForm){
+    if(form.valid){
+      //implement endpoint fetch
+    }else{
+      //implement error message showing
+    }
+  }
 }
