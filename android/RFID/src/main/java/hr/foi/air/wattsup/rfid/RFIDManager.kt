ebc6485c3994a12e815247f10972a4d378010aee
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-11-21 17:35:49 +0100

    ANP-49 Rebase

--- android/RFID/src/main/java/hr/foi/air/wattsup/rfid/RFIDManager.kt
+++ android/RFID/src/main/java/hr/foi/air/wattsup/rfid/RFIDManager.kt
@@ -1,4 +1,37 @@
 package hr.foi.air.wattsup.rfid
 
-class RFIDManager {
+import android.content.Context
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.provider.Settings
+
+class RFIDManager(private val context: Context) {
+
+    private val nfcAdapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(context)
+
+    fun scanRFID(onResult: (RFIDScanResult) -> Unit) {
+        if (nfcAdapter == null) {
+            onResult(RFIDScanResult.NotSupported)
+            return
+        }
+
+        if (!nfcAdapter.isEnabled) {
+            onResult(RFIDScanResult.NFCTurnedOff)
+            openNFCSettings()
+            return
+        }
+    }
+
+    private fun openNFCSettings() {
+        val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+        context.startActivity(intent)
+    }
+}
+
+sealed class RFIDScanResult {
+    object ValidCard : RFIDScanResult()
+    object InvalidCard : RFIDScanResult()
+    object NothingScanned : RFIDScanResult()
+    object NotSupported : RFIDScanResult()
+    object NFCTurnedOff : RFIDScanResult()
 }
