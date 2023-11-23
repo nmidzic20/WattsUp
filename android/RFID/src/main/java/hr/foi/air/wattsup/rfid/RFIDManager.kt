11f21b9bc4c2f8aa74baa52bcfaae4970baf8038
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-11-23 19:05:48 +0100

    ANP-49 Removed unnecessary permissions

--- android/RFID/src/main/java/hr/foi/air/wattsup/rfid/RFIDManager.kt
+++ android/RFID/src/main/java/hr/foi/air/wattsup/rfid/RFIDManager.kt
@@ -1,40 +1,27 @@
 package hr.foi.air.wattsup.rfid
 
-import android.Manifest
 import android.app.Activity
 import android.bluetooth.le.ScanCallback
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.nfc.NfcAdapter
 import android.nfc.NfcManager
-import android.os.Build
 import android.provider.Settings
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ComponentActivity
-import androidx.core.content.ContextCompat
 
 class RFIDManager(
     private val context: Context,
     private val permissionCallback: PermissionCallbackRFID? = null,
 ) {
-
-    private val REQUIRED_PERMISSIONS = arrayOf(
-        Manifest.permission.NFC,
-    )
-
     private val nfcManager = context.getSystemService(Context.NFC_SERVICE) as NfcManager?
     private val nfcAdapter: NfcAdapter? = nfcManager?.defaultAdapter
 
     init {
-        requestRFIDPermissions(context)
         initializeRFID()
     }
 
     companion object {
         private const val REQUEST_ENABLE_RFID = 1
-        private const val REQUEST_PERMISSIONS_RFID = 2
     }
 
     fun isRFIDSupported(): Boolean {
@@ -59,22 +46,6 @@ class RFIDManager(
         }
 
         Log.i("RFID", "RFID supported and enabled")
-    }
-
-
-    private fun requestRFIDPermissions(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val permissionsToRequest = REQUIRED_PERMISSIONS.filter {
-                ContextCompat.checkSelfPermission(context, it) != PackageManager.PERMISSION_GRANTED
-            }
-            if (permissionsToRequest.isNotEmpty()) {
-                ActivityCompat.requestPermissions(
-                    context as Activity,
-                    permissionsToRequest.toTypedArray(),
-                    REQUEST_PERMISSIONS_RFID,
-                )
-            }
-        }
     }
 
     fun showEnableRFIDOption(context: Context) {
