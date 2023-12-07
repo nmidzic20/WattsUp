860c266d759af454d59ca6e3a569888bb1262b7d
Noa Midzic <nmidzic@yahoo.com>
2023-12-07 11:22:41 +0100

    ANP-80 Removed unnecessary interface methods

--- android/core/src/main/java/hr/foi/air/wattsup/core/CardManager.kt
+++ android/core/src/main/java/hr/foi/air/wattsup/core/CardManager.kt
@@ -8,12 +8,6 @@ interface CardManager {
     fun isCardSupportAvailableOnDevice(): Boolean
     fun isCardSupportEnabledOnDevice(): Boolean
     fun showEnableCardSupportOption(context: Context)
-
-    // fun checkAndRequestPermission(permission: String, requestCode: Int)
     fun startScanningForCard(cardScanCallback: CardScanCallback?)
     fun stopScanningForCard()
-    // fun connectToCard(/**/)
-    // fun disconnectFromCard()
-    // fun sendDataToCard(/)
-    // fun readDataFromCard()
 }
