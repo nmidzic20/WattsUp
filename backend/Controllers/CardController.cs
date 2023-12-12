4ae77c3cc96863b7244a6cfe79d316db233daf93
Filip Šoštarić <fsostaric20@foi.hr>
2023-12-12 16:36:39 +0100

    ANP-76 added post, put and delete endpoints for cards, edited CardCreateRequest to fit new endpoints

--- backend/Controllers/CardController.cs
+++ backend/Controllers/CardController.cs
@@ -1,5 +1,6 @@
 ﻿using backend.Data;
 using backend.Models.Entities;
+using backend.Models.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +24,80 @@ namespace backend.Controllers
                 .ToListAsync();
 
             return Ok(res);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Card>> PostCard(CardCreateRequest _card)
+        {
+            try {
+                var res = await _dbContext.Card
+                .Where(c => c.Value == _card.Value)
+                .FirstOrDefaultAsync();
+
+                if (res != null) {
+                    return Conflict("Card with the same value already exists");
+                }
+
+                var card = new Card {
+                    OwnedById = _card.OwnedById,
+                    Value = _card.Value,
+                    Active = true
+                };
+                _dbContext.Card.Add(card);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(card);
+
+            }catch (Exception ex) {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+            
+        }
+
+        [HttpPut("cardId")]
+        public async Task<ActionResult<Card>> PutCard(long cardID, Card _card)
+        {
+            try {
+                var card = await _dbContext.Card
+                .Where(c => c.Id == cardID)
+                .FirstOrDefaultAsync();
+
+                if(card == null) {
+                    return NotFound("Specified card doesn't exist.");
+                }
+
+                card.OwnedById = _card.OwnedById;
+                card.Value = _card.Value;
+                card.Active = _card.Active;
+
+                await _dbContext.SaveChangesAsync();
+
+                return Ok($"Updated card {card.Id}");
+
+            } catch (Exception ex) {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("cardId")]
+        public async Task<ActionResult<Card>> DeleteCard(long cardID) {
+            try {
+                var card = await _dbContext.Card
+                .Where(c => c.Id == cardID)
+                .FirstOrDefaultAsync();
+
+                if (card == null) {
+                    return NotFound("Specified card doesn't exist.");
+                }
+
+                _dbContext.Card.Remove(card);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok($"Deleted card {card.Id}");
+
+            } catch (Exception ex) {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
         }
 
         [HttpGet("{userId}")]
