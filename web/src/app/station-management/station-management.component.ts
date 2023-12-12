72708373d90fb574c4e412b3b1c3860033b90d6d
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-12-12 22:57:03 +0100

    ANP-20 Created UI for button and stations info

--- web/src/app/station-management/station-management.component.ts
+++ web/src/app/station-management/station-management.component.ts
@@ -8,12 +8,16 @@ import { UserManagerService } from '../services/user-manager.service';
   styleUrls: ['./station-management.component.scss']
 })
 export class StationManagementComponent implements OnInit {
+  totalStations = 32;
+  availableStations = 12;
+  unavailableStations = 8;
+  offlineStations = 1;
 
   constructor(private router: Router, private userManagerService: UserManagerService){}
 
   async ngOnInit(): Promise<void> {
     let tokens = this.userManagerService.getTokens();
-
+    /*
     if (tokens) {
       if (await this.userManagerService.validTokens(tokens)) {
         if(tokens.jwtInfo?.role != 'Admin') {
@@ -24,6 +28,6 @@ export class StationManagementComponent implements OnInit {
       }
     } else {
       this.router.navigate(['/login']);
-    }
+    }*/
   }
 }
