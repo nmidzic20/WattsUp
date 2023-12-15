fb3ccd958889ada34706c542f181f5a4ec6c2f5f
Karlo Vardijan <kvardijan20@foi.hr>
2023-12-15 12:29:43 +0100

    ANP-82 Implemented charger adding

--- web/src/app/add-station-dialogue/add-station-dialogue.component.ts
+++ web/src/app/add-station-dialogue/add-station-dialogue.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UserManagerService } from '../services/user-manager.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-station-dialogue',
@@ -8,9 +10,66 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AddStationDialogueComponent {
   @Input() isVisible: boolean = false;
   @Output() closeDialogue: EventEmitter<void> = new EventEmitter<void>();
+  stationNameInput?: HTMLInputElement;
+  longitudeInput?: HTMLInputElement;
+  latitudeInput?: HTMLInputElement;
+
+  constructor(private router: Router, private userManagerService: UserManagerService) { }
 
   close() {
     this.isVisible = false;
     this.closeDialogue.emit();
+  }
+
+  async checkTokenValidity() {
+    let tokens = this.userManagerService.getTokens();
+    if (tokens) {
+      if (await this.userManagerService.validTokens(tokens)) {
+        if (tokens.jwtInfo?.role != 'Admin') {
+          this.router.navigate(['/map']);
+        } else {
+          return;
+        }
+      } else {
+        this.router.navigate(['/login']);
+      }
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  async addStation() {
+    await this.checkTokenValidity();
+
+    this.stationNameInput = document.getElementById("stationName") as HTMLInputElement;
+    this.longitudeInput = document.getElementById("longitude") as HTMLInputElement;
+    this.latitudeInput = document.getElementById("latitude") as HTMLInputElement;
+    let stationName = this.stationNameInput?.value;
+    let longitude = this.longitudeInput?.value;
+    let latitude = this.latitudeInput?.value;
+
+    let tokens = this.userManagerService.getTokens();
+    let createdById = tokens?.jwtInfo?.id;
+
+    let header = new Headers();
+    header.set("Content-Type", "application/json");
+    header.set("accept", "text/plain");
+    header.set("Authorization", "Bearer " + this.userManagerService.getTokens()?.jwt);
+    let body = { name: stationName, latitude: latitude, longitude: longitude, createdById: createdById };
+    console.log(body)
+    let parameters = { method: 'POST', headers: header, body: JSON.stringify(body) };
+
+    try {
+      let response = await fetch("https://localhost:32770/api/Charger", parameters);
+      let body = await response.text();
+      if (response.status == 200) {
+        this.close();
+      } else {
+        let errorMessage = JSON.parse(body).message;
+        window.alert(response.status.toString() + ": " + errorMessage);
+      }
+    } catch (error) {
+      window.alert((error as Error).message);
+    }
   }
 }
