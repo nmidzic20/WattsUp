fff9e559ec3804f98be8b48105cf1adbc14ea3c5
Karlo Vardijan <kvardijan20@foi.hr>
2023-12-16 01:03:30 +0100

    ANP-82 Implemented simple map in popup window

--- web/src/app/add-station-dialogue/add-station-dialogue.component.ts
+++ web/src/app/add-station-dialogue/add-station-dialogue.component.ts
@@ -1,6 +1,20 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { UserManagerService } from '../services/user-manager.service';
 import { Router } from '@angular/router';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import { OSM } from 'ol/source';
+import { fromLonLat, transform } from 'ol/proj';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import { Feature } from 'ol';
+import { Circle, Point } from 'ol/geom';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import Style from 'ol/style/Style';
+import { map } from 'rxjs';
+import Icon from 'ol/style/Icon';
 
 @Component({
   selector: 'app-add-station-dialogue',
@@ -14,6 +28,9 @@ export class AddStationDialogueComponent {
   stationNameInput?: HTMLInputElement;
   longitudeInput?: HTMLInputElement;
   latitudeInput?: HTMLInputElement;
+  dialogueInputContainerIsVisible: boolean = true;
+  dialogueMapIsVisible: boolean = false;
+  map!: Map | null;
 
   constructor(private router: Router, private userManagerService: UserManagerService) { }
 
@@ -73,5 +90,79 @@ export class AddStationDialogueComponent {
     } catch (error) {
       window.alert((error as Error).message);
     }
+  }
+
+  openMap() {
+    this.dialogueInputContainerIsVisible = false;
+    this.dialogueMapIsVisible = true;
+    setTimeout(() => {
+      this.drawMap();
+    }, 200);
+  }
+
+  closeMap() {
+    this.dialogueInputContainerIsVisible = true;
+    this.dialogueMapIsVisible = false;
+
+    if (this.map) {
+      this.map.setTarget(undefined);
+      this.map = null;
+    }
+  }
+
+  drawMap() {
+    const varazdinCoordinates = [16.33778, 46.30444] //longitude, latitude
+
+    if (this.map == null) {
+      this.map = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        target: 'map',
+        controls: [],
+        view: new View({
+          center: fromLonLat(varazdinCoordinates),
+          zoom: 14, maxZoom: 18,
+        }),
+      });
+
+      const vectorSource = new VectorSource();
+      const vectorLayer = new VectorLayer({
+        source: vectorSource,
+      });
+      this.map.addLayer(vectorLayer);
+  
+      // Create a marker at the center of the map
+      const marker = new Feature({
+        geometry: new Point(this.map.getView().getCenter()!!),
+      });
+  
+      // Style for the marker with a PNG image
+      const markerStyle = new Style({
+        image: new Icon({
+          src: '../../assets/MapMarker.png', // Replace with the actual path to your PNG image
+          scale: 0.2, // Adjust the scale as needed
+        }),
+      });
+  
+      
+      marker.setStyle(markerStyle);
+  
+      // Add the marker to the vector layer
+      vectorSource.addFeature(marker);
+
+          // Listen for the map's moveend event
+    this.map.on('postrender', () => {
+      // Update the marker's position to the center of the new map view
+      marker.getGeometry()!!.setCoordinates(this.map!!.getView().getCenter()!!);
+    });
+    } else {
+      this.map.updateSize();
+    }
+
+    
+    
   }
 }
