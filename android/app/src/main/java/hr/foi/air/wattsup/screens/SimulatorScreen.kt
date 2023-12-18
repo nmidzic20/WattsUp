e3f5409770e32921406ada21d47a3c3d28fcd4a4
Noa Midzic <nmidzic@yahoo.com>
2023-12-18 09:34:58 +0100

    ANP-79 Added dropdown menu for charger selection UI

--- android/app/src/main/java/hr/foi/air/wattsup/screens/SimulatorScreen.kt
+++ android/app/src/main/java/hr/foi/air/wattsup/screens/SimulatorScreen.kt
@@ -7,20 +7,30 @@ import android.content.pm.ActivityInfo
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,16 +59,19 @@ fun SimulatorScreen(viewModel: ChargerViewModel, onArrowBackClick: () -> Unit) {
                         Icon(Icons.Filled.ArrowBack, null, tint = Color.White)
                     }
                 },
+                modifier = Modifier
+                    .height(40.dp),
             )
         },
+        containerColor = colorDarkGray,
     ) {
-        Column(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
                 .padding(it),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             CarChargeIndicator(
                 modifier = Modifier
@@ -74,6 +87,49 @@ fun SimulatorScreen(viewModel: ChargerViewModel, onArrowBackClick: () -> Unit) {
                     viewModel.updateInitialChargeAmount(position / 100f)
                 },
             )
+
+            val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
+            var expanded by remember { mutableStateOf(false) }
+            var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {
+                    expanded = !expanded
+                },
+            ) {
+                TextField(
+                    readOnly = true,
+                    value = selectedOptionText,
+                    onValueChange = { },
+                    label = { Text("Label") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded,
+                        )
+                    },
+                    modifier = Modifier.menuAnchor(),
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {
+                        expanded = false
+                    },
+                ) {
+                    options.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text = { Text(text = selectionOption, color = colorDarkGray) },
+                            onClick = {
+                                selectedOptionText = selectionOption
+                                expanded = false
+                            },
+                            modifier = Modifier
+                                .background(MaterialTheme.colorScheme.secondary),
+                        )
+                    }
+                }
+            }
         }
     }
 }
