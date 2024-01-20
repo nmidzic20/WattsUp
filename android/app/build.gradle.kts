7a6a7f61417ddf91a514260493346470e6759662
Hrvoje Lukšić <hluksic20@foi.hr>
2024-01-20 18:58:00 +0100

    ANP-91 added bad login UI test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0-beta01")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
--- android/app/src/androidTest/java/hr/foi/air/wattsup/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/hr/foi/air/wattsup/ExampleInstrumentedTest.kt
@@ -1,36 +1,34 @@
 package hr.foi.air.wattsup
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.junit.Rule
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class UITest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun loginTest() {
-        composeTestRule.onNodeWithText("User mode").performClick()
+    fun wrongLoginTest() {
+        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
-        composeTestRule.onNodeWithText("Username").performTextInput("a")
+        composeTestRule.onNodeWithText("User mode").performClick()
+        composeTestRule.onNodeWithText("E-mail").performTextInput("a")
         composeTestRule.onNodeWithText("Password").performTextInput("a")
-
-        composeTestRule.onNodeWithText("User mode").performClick()
-
+        composeTestRule.onAllNodesWithText("Login")[0].performClick()
+        Thread.sleep(500)
+        composeTestRule.waitForIdle()
+        assertTrue(uiDevice.hasObject(By.text("Login")))
     }
 }
